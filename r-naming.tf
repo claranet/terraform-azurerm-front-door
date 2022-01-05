122894c23093369d2a39a3d6efaf70136e5833a6
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-01-05 16:21:13 +0100

    AZ-515: No location for naming with FD

--- r-naming.tf
+++ r-naming.tf
@@ -2,7 +2,7 @@ resource "azurecaf_name" "frontdoor" {
   name          = var.stack
   resource_type = "azurerm_frontdoor"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, var.use_caf_naming ? "" : "fd"])
+  suffixes      = compact([var.client_name, var.environment, local.name_suffix, var.use_caf_naming ? "" : "fd"])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
@@ -12,7 +12,7 @@ resource "azurecaf_name" "frontdoor_probe" {
   name          = var.stack
   resource_type = "azurerm_frontdoor"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, "probe", var.use_caf_naming ? "" : "fd"])
+  suffixes      = compact([var.client_name, var.environment, local.name_suffix, "probe", var.use_caf_naming ? "" : "fd"])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
@@ -22,7 +22,7 @@ resource "azurecaf_name" "frontdoor_lb" {
   name          = var.stack
   resource_type = "azurerm_frontdoor"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, "lb", var.use_caf_naming ? "" : "fd"])
+  suffixes      = compact([var.client_name, var.environment, local.name_suffix, "lb", var.use_caf_naming ? "" : "fd"])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
